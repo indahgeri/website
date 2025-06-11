97431e719e7002496e9f766ab0ebcb15f0f5c3fa
elang surya <langsurya5@gmail.com>
2025-06-11 14:41:01 +0700

    Hilangkan maximum-scale=1.0 dan user-scalable=no

--- dashboard/app/Views/invitation/index.php
+++ dashboard/app/Views/invitation/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Selamat datang di undangan digital Indah dan Gery. Lihat waktu & lokasi acara, konfirmasi kehadiran, dan sampaikan ucapan melalui halaman ini.">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
